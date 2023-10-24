30cfc55c4c7eb23c0d12fcd048027eee5e900b0e
horus <h.horace0921@gmail.com>
2023-10-24 18:09:12 +0800

    feat: allow auto email link for trusted providers

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -108,10 +108,12 @@ export const authOptions: NextAuthOptions = {
     GitHubProvider({
       clientId: env.GITHUB_CLIENT_ID,
       clientSecret: env.GITHUB_CLIENT_SECRET,
+      allowDangerousEmailAccountLinking: true,
     }),
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
+      allowDangerousEmailAccountLinking: true,
     }),
     /**
      * ...add more providers here.
