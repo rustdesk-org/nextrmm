07a61bb6545bad9e1f08a8e4256f0846a4bceb89
Mocus EZ <285918468@qq.com>
2023-10-25 10:31:36 +0800

    fix: sidebar shrinkage fail

--- src/components/body-layout.tsx
+++ src/components/body-layout.tsx
@@ -16,21 +16,18 @@ export function BodyLayout({
   }: {
     children: React.ReactNode;
   }){
-    const [showAside, setShowAside] = React.useState<string>("200px")
     const [isOpen, setIsOpen] = React.useState(false)
 
     function SideNav(){
         if(isOpen){
-            setShowAside("50px");
             return(<AsideHiddenNav items={controlboardConfig.sidebarNav}/>)
         }else{
-            setShowAside("200px");
             return(<AsideShowNav items={controlboardConfig.sidebarNav}/>)
         }
     }
 
-    return(
-        <div className={`container grid flex-1 gap-3 md:grid-cols-[${showAside}_1fr]  max-w-none px-0`}>
+    function SideWidthShort(){
+        return(<div className="container grid flex-1 gap-3 md:grid-cols-[50px_1fr]  max-w-none px-0">
             <aside className="hidden md:flex">
                 <Collapsible open={isOpen} onOpenChange={setIsOpen} className="fixed h-full">
                     <div>
@@ -54,6 +51,39 @@ export function BodyLayout({
                     {children}
                 </main>
             </div>
-        </div>
-    )
+        </div>)
+    }
+
+    function SideWidthLong(){
+        return(<div className="container grid flex-1 gap-3 md:grid-cols-[200px_1fr]  max-w-none px-0">
+            <aside className="hidden md:flex">
+                <Collapsible open={isOpen} onOpenChange={setIsOpen} className="fixed h-full">
+                    <div>
+                        <SideNav/>
+                    </div>
+                    <div className="flex flex-col-reverse h-[35%]">
+                            <CollapsibleTrigger asChild>
+                                <Button variant="ghost" size="sm">
+                                    <Icons.asideSwitch className="h-4 w-4" />
+                                    <span className="sr-only">Toggle</span>
+                                </Button>
+                            </CollapsibleTrigger>
+                    </div>
+                </Collapsible>
+            </aside>
+            <div className="flex min-h-screen flex-col space-y-6">
+                <header className="sticky top-0 z-40 border-b bg-background">
+                    <HeadNav />
+                </header>
+                <main className="flex w-full flex-1 flex-col overflow-hidden">
+                    {children}
+                </main>
+            </div>
+        </div>)
+    }
+
+    if (isOpen) {
+        return <SideWidthShort/>;
+    }
+    return <SideWidthLong/>;
 }
