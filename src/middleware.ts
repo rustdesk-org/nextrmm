31b0c1f4a92a6286d9c4fa2bd97cb26cce85804a
horus <h.horace0921@gmail.com>
2023-10-31 11:06:27 +0800

    fix: ignore public files for i18n redirecting

--- src/middleware.ts
+++ src/middleware.ts
@@ -25,6 +25,17 @@ function getLocale(request: NextRequest): string | undefined {
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
 
+  // // `/_next/` and `/api/` are ignored by the watcher, but we need to ignore files in `public` manually.
+  if (
+    [
+      "/favicon.ico",
+      "/favicon-16x16.png",
+      "/favicon-32x32.png",
+      "/nextrmm.svg",
+    ].includes(pathname)
+  )
+    return;
+
   // Check if there is any supported locale in the pathname
   const pathnameIsMissingLocale = i18n.locales.every(
     (locale) =>
