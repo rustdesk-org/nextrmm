20974e4b66de99ba0ecc780d7d426275de24b051
horus <h.horace0921@gmail.com>
2023-11-02 10:28:16 +0800

    feat: add top nav for auth pages

--- src/app/[locale]/(auth)/layout.tsx
+++ src/app/[locale]/(auth)/layout.tsx
@@ -1,3 +1,38 @@
+import Link from "next/link";
+import { Icons } from "~/components/icons";
+import { buttonVariants } from "~/components/ui/button";
+import { siteConfig } from "~/config/site";
+import { cn } from "~/lib/utils";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <main className="min-w-screen min-h-screen">{children}</main>;
+  return (
+    <>
+      <div className="absolute top-0 mx-auto w-full px-8 pt-6 sm:px-6 lg:px-8">
+        <nav className="flex items-center justify-between">
+          <div className="flex shrink-0 grow items-center md:pl-4 lg:grow-0">
+            <Link
+              href="/"
+              className="flex w-full items-center space-x-2 md:w-auto"
+            >
+              <Icons.logo />
+              <span className="font-bold">{siteConfig.name}</span>
+            </Link>
+          </div>
+          <div className=" hidden items-center md:ml-10 md:flex md:pr-4">
+            <Link
+              href="/document"
+              className={cn(
+                buttonVariants({ variant: "ghost" }),
+                "flex items-center justify-center space-x-2",
+              )}
+            >
+              <Icons.bookText size={15} />
+              <span>Document</span>
+            </Link>
+          </div>
+        </nav>
+      </div>
+      <main className="min-w-screen min-h-screen">{children}</main>
+    </>
+  );
 }
