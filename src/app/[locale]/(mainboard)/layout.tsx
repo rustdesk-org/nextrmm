36d426fb1df58a8ac30c925f260adc82539e58e1
Mocus EZ <285918468@qq.com>
2023-11-16 11:07:49 +0800

    feature: force login

--- src/app/[locale]/(mainboard)/layout.tsx
+++ src/app/[locale]/(mainboard)/layout.tsx
@@ -1,7 +1,19 @@
+import { redirect } from "next/navigation";
 import { BodyLayout } from "~/components/body-layout";
 import { SiteFooter } from "~/components/site-footer";
+import { getServerAuthSession } from "~/server/auth";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  //Check if user is logged in
+  const session = await getServerAuthSession();
+  if (!session) {
+    redirect("/login");
+  }
+
   return (
     <>
       <BodyLayout>{children}</BodyLayout>
